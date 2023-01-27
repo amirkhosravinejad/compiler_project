b01c437dddc0dc4ee0d9a90262433f4e58b73ddb
Amir Khosravinejad <khosiamir1@gmail.com>
2023-01-27 18:33:02 +0330

    while, if, if-else and assignment backpatching handled!

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
-t_MOD = 'mod'
+t_MOD = r'%'
 # relative operators
 t_LT = '<'
 t_GT = '>'
@@ -138,23 +138,21 @@ class S:
 
 quadruples = []
 
-is_not_bool = False
+primary_var_names = []
+temp_var_names = []
 
 # tl_or_fl is for checking false list or truelists (traversal)
 def backpatch(l, i, tl_or_fl):
-        # for line_number in l:
-        #    quadruples[line_number - 1] = ("goto", i)
-        # print(type(l))
         if (type(l) == list):
             for line_number in l:
-                quadruples[line_number - 1] = ("goto", i)
-        else:
+                replaced_line = quadruples[line_number - 1].replace("_", str(i))
+                quadruples[line_number - 1] = replaced_line
+        elif isinstance(l, E):
             if tl_or_fl == False:
                 child = l.falselist
             else:
                 child = l.truelist    
             
-            # print(truel)
             while type(child) != list:
                 if tl_or_fl == False:
                     child = child.falselist
@@ -163,11 +161,17 @@ def backpatch(l, i, tl_or_fl):
             for line_number in child:
                 replaced_line = quadruples[line_number - 1].replace("_", str(i))
                 quadruples[line_number - 1] = replaced_line
+        elif isinstance(l, S):
+            while type(l) != list:
+                l = l.nextlist
+            for line_number in l:
+                replaced_line = quadruples[line_number - 1].replace("_", str(i))
+                quadruples[line_number - 1] = replaced_line            
                 #print(line_number, i)
         #print(quadruples)
 
 # function for merging truelist or falselist of expressions       
-def merge(E_obj1, E_obj2, tl_or_fl):
+def merge(E_obj1, E_obj2, tl_or_fl: bool):
     # tl_or_fl as we did in backpatch function, is a flag
     # for checking truelist or falselists
     if tl_or_fl == False:
@@ -214,11 +218,49 @@ def merge(E_obj1, E_obj2, tl_or_fl):
 
 def nextinstr():
     return len(quadruples) + 1
-
+    #
 def p_marker(p):
     'marker : '
     p[0] = nextinstr()
 
+def p_n(p):
+    'n : '
+    # N.nlist = makelist(next)
+    p[0] = S([nextinstr()])
+    quadruples.append('goto _')
+
+# this merge is for if statement (without else)
+# which merges the falselist of the boolean expression
+# after 'if' with the nextlist of statement which
+# should jumped into when the boolean expression is false
+def merge_falselist_with_nextlist(falselist, nextlist):
+    #S_next_list = S()
+    if type(falselist) != list:
+        false_l = falselist.falselist
+        while (type(false_l) != list):
+            false_l = false_l.falselist
+    else:
+        
+        false_l = falselist
+    if type(nextlist)!= list:
+        next_l = nextlist.nextlist
+        while (type(next_l)!= list):
+            next_l = next_l.nextlist
+    else:
+            # for NOT expression
+        next_l = nextlist
+        # merge two lists in a new list and return
+        # as the falselist of a new object of E class        
+    temp = false_l + next_l
+    return S(temp)    
+
+def merge_nextlists(s1_nlist, s2_nlist, n_nlist):
+    while (type(s1_nlist) != list and
+     type(s2_nlist) != list and type(n_nlist) != list):
+        s1_nlist = s1_nlist.nextlist
+        s2_nlist = s2_nlist.nextlist
+        n_nlist = n_nlist.nextlist
+    return s1_nlist + s2_nlist + n_nlist   
 
 # def p_program(p):
 #     '''program : PROGRAM IDENTIFIER declarations compoundStatement
@@ -304,58 +346,88 @@ def p_marker(p):
 #         p[0] = p[1] + [p[3]]
 
 
-# # here we have statement rules
-# def p_statement_assignment(p):
-#     '''
-#     statement : IDENTIFIER ASSIGN expression
-#     '''
-#     p[0] = (p[1], p[2], p[3])
+# here we have statement rules
+def p_statement_assignment(p):
+    '''
+    statement : IDENTIFIER ASSIGN expression
+    '''
+    arithmetic_symbols = ['+', '*', '-', '/', '%']
+    # first part of assignment is a nextlist which firstly
+    # points to a blank list 
+    p[0] = (S([]), p[1], p[2], p[3])
+    # when an exression is done (specially for arithmetic one)
+    # if (len(temp_var_names) > 1):
+    #     temp_var_names.pop()
+    #print(p[3])
+    if p[3] in arithmetic_symbols:
+        last = quadruples[len(quadruples) - 1]
+        #print("last: ", last)
 
-#     #pass
+    else:
+        primary_var_names.append(p[1])
+        quadruples.append(p[1] + ' := ' + str(p[3]))
+        return   
+    id_end_index = last.find(" =")
+    #print("index: ", id_end_index)
+    last = last.replace('=', p[2])
+    #print("string: ", last)
+    quadruples[len(quadruples) - 1] = p[1] + last[id_end_index:]
+    #quadruples.append(p[1] + p[2] + str(p[3]))
+    # print("salap")
+    # print(quadruples)
+    # print(temp_var_names)
+    # print("bye")
+    #pass
 
-# def p_statement_if(p):
-#     '''
-#     statement : IF expression THEN statement
-#               | IF expression THEN statement ELSE statement
-#     '''
+def p_statement_if(p):
+    '''
+    statement : IF expression THEN marker statement
+              | IF expression THEN marker statement n ELSE marker statement
+    '''
+    # S -> if E then M S1
+    if len(p) == 6:
+        # backpatch(E.tlist, M.quad)
+        backpatch(p[2], p[4], True)
+        # S.nlist = merge(E.flist, S1.nlist)
+        nextlist = merge_falselist_with_nextlist(p[2].falselist, p[5][0].nextlist)
+        p[0] = (S(nextlist), 'if', p[2], p[5])
+        #quadruples.append()
 
-#     if len(p) == 5:
-#         #p[0] = ('if', (p[2], p[4]))
-#         truelist = p[2].truelist
-#         backpatch(truelist,nextinstr())
-#         nextlist = p[2].falselist + p[5].nextlist
-#         S(nextlist)
+    # S -> if E then M1 S1 n else M2 S2
+    elif len(p) == 10:
+        #p[0] = ('if-else', (p[2], p[4], p[6]))
+        
+        # backpatch(E.tlist, M1.quad)    
+        backpatch(p[2], p[4], True)
+        # backpatch(E.flist, M2.quad)
+        backpatch(p[2], p[8], False)
+        
+        nextlist = merge_nextlists(p[5][0].nextlist, p[9][0].nextlist,
+            p[6].nextlist)
+        p[0] = (S(nextlist), 'if-else', p[2], p[5], p[9])
 
-#     elif len(p) == 7:
-#         #p[0] = ('if-else', (p[2], p[4], p[6]))
 
-# # This Needs Serious Change
-#         truelist = p[2].truelist
-#         faleslist = p[2].falselist
-#         backpatch(truelist,nextinstr())
-#         backpatch(faleslist,nextinstr())
-#         nextlist = p[5].nextlist
-#         #temp = nextlist + N.nextlist !!!!!
-#         S(nextlist)
+def p_statement_while(p):
+    '''
+    statement : WHILE marker expression DO marker statement
+    '''
+    # backpatch(S.nextlist, M1.quad)
+    #print(p[6][0].nextlist)
+    backpatch(p[6][0].nextlist, p[2], True)
+    # backpatch(E.truelist, M2.quad)
+    backpatch(p[3], p[5], True)
+    nextlist = p[3].falselist
+    p[0] = (S(nextlist), p[1], p[3], p[4], p[6])
+    quadruples.append('goto ' + str(p[2]))
+    #pass
 
-# def p_statement_while(p):
-
-#     '''
-#     statement : WHILE marker expression DO marker statement
-#     '''
-
-#     p[0] = ('while', (p[2], p[4]))
-
-#     #pass
-
-# def p_statement_print(p):
-#     '''
-#     statement : PRINT LPAREN expression RPAREN
-#     '''
-
-#     p[0] = ('print', (p[3]))
-
-#     #pass
+def p_statement_print(p):
+    '''
+    statement : PRINT LPAREN expression RPAREN
+    '''
+    p[0] = ('print', (p[3]))
+    quadruples.append('print ' + p[2] + str(p[3]) + p[4])
+    #pass
 
 # rules which expression is in the rule's leftside
 def p_expression_int(p):
@@ -376,7 +448,9 @@ def p_expression_IDENTIFIER(p):
     '''
     expression : IDENTIFIER
     '''
-    p[0] = ('IDENTIFIER', p[1])
+    # p[0] = ('IDENTIFIER', p[1])
+    p[0] = p[1]
+    primary_var_names.append(p[1])
 
 # Write functions for each grammar rule which is
 # specified in the docstring.
@@ -393,16 +467,39 @@ def p_expression_arithmetic(p):
     # expression : expression arithmetic_operaion expression
     #   p[0]     : p[1] p[2] p[3]
     #
-    if p[2] == '+':
+    if (p[2] == '+' or p[2] == '-' or p[2] == '*' 
+        or p[2] == '/' or p[2] == '%'):
         p[0] = (p[1], p[2], p[3])
-    elif p[2] == '-':
-        p[0] = (p[1], p[2], p[3])
-    elif p[2] == '*':
-        p[0] = (p[1], p[2], p[3])
-    elif p[2] == '/':
-        p[0] = (p[1], p[2], p[3])
-    elif p[2] == 'mod':
-        p[0] = (p[1], p[2], p[3])
+        if ((type(p[1]) == tuple or isinstance(p[1], E)) and
+            (type(p[3]) == tuple or isinstance(p[3], E))):
+            op1 = temp_var_names[len(temp_var_names) - 1]
+            op2 = temp_var_names[len(temp_var_names) - 2]
+        else:
+            if type(p[1]) == tuple or isinstance(p[1], E):
+                op1 = temp_var_names[len(temp_var_names) - 1]
+            else:
+                op1 = str(p[1])
+
+            if type(p[3]) == tuple or isinstance(p[3], E):
+                op2 = temp_var_names[len(temp_var_names) - 1]
+            else:
+                op2 = str(p[3])
+
+        temp_var_name = 'temp_int_' + str(len(temp_var_names) + 1)
+        temp_var_names.append(temp_var_name)
+        
+        quadruples.append(temp_var_name + ' = ' + op1 + p[2] + op2)
+        print(f'primary_var_names:{primary_var_names}, temps:{temp_var_names}')
+        print(f'quadruples:{quadruples}, p[0]:{p[0]}')
+
+    # elif p[2] == '-':
+    #     p[0] = (p[1], p[2], p[3])
+    # elif p[2] == '*':
+    #     p[0] = (p[1], p[2], p[3])
+    # elif p[2] == '/':
+    #     p[0] = (p[1], p[2], p[3])
+    # elif p[2] == 'mod':
+    #     p[0] = (p[1], p[2], p[3])
 
 def p_expr_Relop(p):
     '''
@@ -418,7 +515,8 @@ def p_expr_Relop(p):
     # expression : expression relational operaion expression
     #   p[0]     : p[1] p[2] p[3]
     #
-    if p[2] == '<' or p[2] == '=' or p[2] == '>' or p[2] == '<=' or p[2] == '>=' or p[2] == '<>':
+    if (p[2] == '<' or p[2] == '=' or p[2] == '>' or
+        p[2] == '<=' or p[2] == '>=' or p[2] == '<>'):
         #p[0] = (p[1], p[2], p[3])
         truelist = E([nextinstr()], [])
         falselist = E([], [nextinstr() + 1])
@@ -459,7 +557,8 @@ def p_expr_bool_dual(p):
 
 def p_expr_uminus(p):
     'expression : MINUS expression %prec UMINUS'
-    p[0] = ('-',p[2])
+    #p[0] = ('-',p[2])
+    p[0] = '-' + p[2]
 
 def p_expression_NOT(p):
     '''
@@ -485,14 +584,17 @@ def p_expression_grouped(p):
     '''
     expression : LPAREN expression RPAREN
     '''
-    #p[0] = ('grouped', p[2])
-    p[0] = E(p[2].truelist, p[2].falselist)
+    #p[0] = E(p[2].truelist, p[2].falselist)
+    if (isinstance(p[2], E)):
+        p[0] = E(p[2].truelist, p[2].falselist)
+    else:
+        p[0] = (p[2])    
 
 def p_error(p):
     print(f'Syntax error at {p.value!r}')
 
 # Build the parser
-parser = yacc(start='expression')
+parser = yacc(start='statement')
 
 #data = 'var sam, tiare, pain : int; a,b,c:real'
 # input = '''program iliare
@@ -507,10 +609,12 @@ parser = yacc(start='expression')
 #     if a * c && ! d then
 #         print(f)
 # end'''
-input = '(!(!(e < f))) && (salam = kh) || (22 <> m)'
+#input = '(!(!(e < f))) && (salam = kh) || (22 <> m)'
+input = 'while 3 = 5 do if 3 <> 4 then x:= z else x := y % 4'
 
 # Parse an expression
-ast = parser.parse(input, debug=False)
+ast = parser.parse(input, debug=True)
 print(ast)
 for i in quadruples:
     print(i)
+print(f'primary_var_names:{primary_var_names}, temps:{temp_var_names}')    
